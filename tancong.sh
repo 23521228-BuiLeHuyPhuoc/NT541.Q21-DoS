edb7abcceaaff8f86e58b80fb559626c95c3a29c
Manhx2 <ngothimaianh0401@gmail.com>
2026-04-18 22:34:04 -0700

    Cap nhat file tancong.sh moi nhat tu branch Sang tren server

--- tancong.sh
+++ tancong.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
-# Script này chạy trên các host của Mininet
-
-DES_IP="10.0.2.10"
-
-echo "Khoi dong server tren h_web"
-iperf -s -p 80 &
-sleep 2
-
-echo "2. Nguoi dung truuy cap vao web"
-iperf -c $DES_IP -p 80 -t 300 &
-sleep 10 
-
-echo "3. Tan cong SYN Flood"
-hping3 -S -p 80 --flood $DES_IP
-
-
+h_web1 iperf -s -p 80 &
+sh sleep 2
+h_ext1 iperf -c 10.0.2.10 -p 80 -t 300 &
+sh sleep 5
+h_att1 hping3 -S -p 80 --flood 10.0.2.10
