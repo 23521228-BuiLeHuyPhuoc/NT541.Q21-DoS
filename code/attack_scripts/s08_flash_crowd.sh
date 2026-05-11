1dde5e769a9945db086fc2edde60b8f64d1144fa
Phuoc <23521228@gm.uit.edu.vn>
2026-05-11 22:55:07 +0700

    sửa demo 8

--- code/attack_scripts/s08_flash_crowd.sh
+++ code/attack_scripts/s08_flash_crowd.sh
@@ -15,29 +15,14 @@ echo "Mo phong nhieu nguoi dung hop phap truy cap dong thoi"
 # Phien 1: Web port 80
 timeout $DURATION hping3 -S -p 80   -i u50000 $VICTIM &
 PID1=$!
-sleep 0.1
-
-# Phien 2: HTTPS port 443
 timeout $DURATION hping3 -S -p 443  -i u50000 $VICTIM &
 PID2=$!
-sleep 0.1
-
-# Phien 3: API port 8080
 timeout $DURATION hping3 -S -p 8080 -i u50000 $VICTIM &
 PID3=$!
-sleep 0.1
-
-# Phien 4: Web khac port 8888
 timeout $DURATION hping3 -S -p 8888 -i u50000 $VICTIM &
 PID4=$!
-sleep 0.1
-
-# Phien 5: SSH port 22
 timeout $DURATION hping3 -S -p 22   -i u50000 $VICTIM &
 PID5=$!
-sleep 0.1
-
-# Phien 6: FTP port 21
 timeout $DURATION hping3 -S -p 21   -i u50000 $VICTIM &
 PID6=$!
 
